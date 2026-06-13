799141f0769049bd6151ec8527762a08e9c0cf06
abhind-collab <abhinand@veuz.in>
2026-06-13 10:24:58 +0530

    updated start command

--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+
+python manage.py migrate &&
+python manage.py create_admin &&
+python manage.py collectstatic --noinput &&
 celery -A config worker --loglevel=info &
 celery -A config beat --loglevel=info &
 
