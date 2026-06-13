69aafb7962b12eda5d3d5cfa6a35ec0f603985cf
abhind-collab <abhinand@veuz.in>
2026-06-13 10:57:02 +0530

    updated bash

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-
 python manage.py migrate &&
 python manage.py create_admin &&
 python manage.py collectstatic --noinput &&
-celery -A config worker --loglevel=info &
+
+celery -A config worker --loglevel=info --concurrency=1 --pool=solo &
 celery -A config beat --loglevel=info &
 
 gunicorn config.wsgi:application --workers 1
